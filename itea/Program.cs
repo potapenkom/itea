4f8b019b0edf9c68e912737a1c47f759d0a79693
Mikhail Potapenko <it.potapenkom@gmail.com>
2019-09-17 23:53:16 +0300

    first commit

---
+++ itea/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace itea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
