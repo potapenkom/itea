1150c96b8d6d9ee5f38ceac7ff1cbc82384b669d
Mikhail Potapenko <it.potapenkom@gmail.com>
2019-09-29 22:25:04 +0300

    added a solution to seond task

--- itea/Program.cs
+++ itea/Program.cs
@@ -17,6 +17,29 @@ namespace itea
             string patronymic = fullName.Substring(lastSpace + 1, 1).ToUpper();
             Console.WriteLine($"Your short name is {surname } {name} {patronymic}");
             /*end first task*/
+            /*second task*/
+            Console.Write("Enter your full name: ");
+            string fullName2 = Console.ReadLine();
+            int firstSpace2 = fullName2.IndexOf(" ");
+            int lastSpace2 = fullName2.LastIndexOf(" ");
+            string surname2 = fullName2.Substring(0, firstSpace);
+            string partName = fullName2.Substring(firstSpace + 1);
+            string name2 = partName.Substring(0, firstSpace);
+            string patronymic2 = fullName2.Substring(lastSpace + 1);
+            string[,] full = {{surname2, name2, patronymic2},
+                       {surname2, name2, patronymic2},  
+                       {surname2, name2, patronymic2},};
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write(full[i, j]);
+                }
+                Console.WriteLine();
+            }
+
+            /*end second task*/
+
         }
     }
 }
