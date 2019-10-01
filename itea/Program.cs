133733ac1cda3293e7595d93db543d784076876f
Mikhail Potapenko <it.potapenkom@gmail.com>
2019-10-01 15:24:47 +0300

    new solution to second task

--- itea/Program.cs
+++ itea/Program.cs
@@ -16,54 +16,52 @@ namespace itea
             string name = fullName.Substring(firstSpace + 1, 1).ToUpper();
             string patronymic = fullName.Substring(lastSpace + 1, 1).ToUpper();
             Console.WriteLine($"Your short name is {surname } {name} {patronymic}");
-            /*end first task*/
+           /* end first task*/
             /*second task*/
-            Console.Write("Enter your full name: ");
-            string fullName1 = Console.ReadLine();
-            int firstSpace1 = fullName1.IndexOf(" ");
-            int lastSpace1 = fullName1.LastIndexOf(" ");
-            string surname1 = fullName.Substring(0, firstSpace1);
-            string partName = fullName.Substring(firstSpace1 + 1);
-            int firstSpace2 = partName.IndexOf(" ");
-            string name1 = partName.Substring(0, firstSpace2);
-            string patronymic1 = fullName.Substring(lastSpace1 + 1);
-
-            char[] surnameArr = surname1.ToCharArray();
-            char[] nameArr = name1.ToCharArray();
-            char[] patronymicArr = patronymic1.ToCharArray();
-            char[][] fullNameArr = new char[3][];
-
-            fullNameArr[0] = new char[surnameArr.Length];
-            fullNameArr[1] = new char[nameArr.Length];
-            fullNameArr[2] = new char[patronymicArr.Length];
-
-            int i;
-            for (i = 0; i < surnameArr.Length; i++)
-                fullNameArr[0][i] = surnameArr[i];
-            for (i = 0; i < nameArr.Length; i++)
-                fullNameArr[1][i] = nameArr[i];
-            for (i = 0; i < patronymicArr.Length; i++)
-                fullNameArr[2][i] = patronymicArr[i];
-
-            for (i = 0; i < surnameArr.Length; i++)
-                Console.Write(fullNameArr[0][i] + " ");
+            string[][] fullNameArr = new string[3][];
+            for (int k = 0; k < fullNameArr.Length; k++)
+            {
+                Console.Write("Enter your full name: ");
+                string fullName1 = Console.ReadLine();
+                int firstSpace1 = fullName1.IndexOf(" ");
+                int lastSpace1 = fullName1.LastIndexOf(" ");
+                string surname1 = fullName1.Substring(0, firstSpace1);
+                string partName = fullName1.Substring(firstSpace1 + 1);
+                int firstSpace2 = partName.IndexOf(" ");
+                string name1 = partName.Substring(0, firstSpace2);
+                string patronymic1 = fullName1.Substring(lastSpace1 + 1);
+                for (int i = 0; i < fullNameArr.Length; i++)
+                {
+                    fullNameArr[i] = new string[] { surname1, name1, patronymic1 };
+                }
+                }
+                for (int i = 0; i < fullNameArr.Length; i++)
+                {
+                for (int j = 0; j < fullNameArr[i].Length; j++)
+                {
+                    Console.Write(fullNameArr[i][j] + "\t");
+                }
                 Console.WriteLine();
-
-            for (i = 0; i < nameArr.Length; i++)
-                Console.Write(fullNameArr[1][i] + " ");
-                Console.WriteLine();
-
-            for (i = 0; i < patronymicArr.Length; i++)
-                Console.Write(fullNameArr[2][i] + " ");
-                Console.WriteLine();
+            }
+            Console.WriteLine();
 
             Console.Write("Enter a string to search for: ");
             string search = Console.ReadLine();
-            if (fullName1.StartsWith(search) == true)
+            for (int i = 0; i < fullNameArr.Length; i++)
             {
-                Console.WriteLine("Substring found!");
+                for (int j = 0; j < fullNameArr[i].Length; j++)
+                {
+                    string str = fullNameArr[i][j];
+                    if (str.IndexOf(search) != -1)
+                    {
+                        Console.WriteLine("Substring " + search + " found!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Substring not found!");
+                    }
+                }
             }
-                Console.WriteLine("Substring not found!");
 
             /*end second task*/
 
