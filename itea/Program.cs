ca0a54b6bf925b617a5a2415352bc921086992c1
Mikhail Potapenko <it.potapenkom@gmail.com>
2019-10-05 20:38:31 +0300

    added removeProduct method

--- itea/Program.cs
+++ itea/Program.cs
@@ -2,102 +2,82 @@
 
 namespace itea
 {
-    class Student
+    class Product
     {
-        private bool IsDrunk;
-        public void setIsDrunk(bool state)
+        string name;
+        int price;
+        public Product(string name, int price)
         {
-            IsDrunk = state;
+            this.name = name;
+            this.price = price;
         }
-        public bool getIsDrunk()
+        public string getName()
         {
-            return IsDrunk;
+            return name;
         }
-        private int studentTails;
-        public void setStudentTails(int tails)
+        public int getPrice()
         {
-            studentTails = tails;
+            return price;
         }
-        public int getStudentTails()
-        {
-            return studentTails;
-        }
-
-        private int studentIq;
-        public void setStudentIq(int iq)
-        {
-            studentIq = iq;
-        }
-        public int getStudentIq()
-        {
-            return studentIq;
-        }
-        private string studentName;
-        public void setStudentName()
-        {
-            Console.Write("Enter your name: ");
-            studentName = Console.ReadLine();
-        }
-        public string getStudentName()
-        {
-            return studentName;
-        }
-
-        private string studentFaculty;
-        public void setStudentFaculty()
-        {
-            Console.Write("Enter your faculty: ");
-            studentFaculty = Console.ReadLine();
-        }
-        public string getStudentFaculty()
-        {
-            return studentFaculty;
-        }
-
-        public void getInfo()
-        {
-            Console.WriteLine("Student name is " + studentName + " Student studying at the faculty " + 
-                studentFaculty + ". Today he is " + (IsDrunk ? "drunk" : "sober") + " Iq level is equal- " 
-                + studentIq + " The student has "  + studentTails + " tails");
-        }
-
     }
+
+    class Cart
+    {
+        Product[] products = new Product[10];
+        public void addProduct(Product product)
+        {
+            for (int i = 0; i < products.Length; i++)
+            {
+                if (products[i] == null)
+                {
+                    products[i] = product;
+                    break;
+                }
+            }
+        }
+        public void removeProduct(Product product)
+        {
+            for (int i = 0; i < products.Length; i++)
+            {
+                if (products[i].getName() == product.getName())
+            {  
+                    products[i] = null;
+                    break;
+                }
+            }
+        }
+        public void showCart()
+        {
+            Console.WriteLine("CART:");
+            int total = 0;
+            for (int i = 0; i < products.Length; i++)
+            {
+                if (products[i] != null)
+                {
+                    Console.WriteLine("\t" + products[i].getName() + " : " + products[i].getPrice());
+                    total += products[i].getPrice();
+                }
+            }
+            Console.WriteLine("\t\tCART TOTAL: " + total);
+        }
+    }
+
     class Program
     {
         public static void Main(string[] args)
         {
-            Random rnd = new Random();
-            int iqLevel = rnd.Next(1, 155);
-            int tailCount = rnd.Next(0, 8);
-            bool result = rnd.Next(100) < 50 ? true : false;
-            Student student = new Student();
-            student.setStudentName();
-            student.setStudentFaculty();
-            student.setIsDrunk(result);
-            student.setStudentTails(tailCount);
-            student.setStudentIq(iqLevel);
-            student.getInfo();
-            bool IsDrunk = student.getIsDrunk();
-            int iq = student.getStudentIq();
-            int tails = student.getStudentTails();
-            if (IsDrunk == true && iq < 70 || IsDrunk == true && tails > 3 || iq < 70 && tails > 3)
-            {
-                Console.WriteLine("Student expelled");
-            }
-            else
-            {
-                student.setIsDrunk(result);
-                IsDrunk = student.getIsDrunk();
-                student.getInfo();
-                if (IsDrunk == false)
-                {
-                    iq += 10;
-                    student.getInfo();
-                }
-                student.getInfo();
-            }
+               Product p1 = new Product("Coca-Cola", 15);
+               Product p2 = new Product("Pepsi-Cola", 17);
+               Product p3 = new Product("Milk", 26);
+               Product p4 = new Product("Jack Daniels Fire", 450);
+               Cart cart = new Cart();
+               cart.addProduct(p1);
+               cart.addProduct(p3);
+               cart.addProduct(p4);
+               cart.showCart();
+               cart.removeProduct(p4);
+               cart.showCart();
         }
-
     }
 }
 
